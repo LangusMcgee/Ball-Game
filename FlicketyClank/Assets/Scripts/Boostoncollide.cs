37810c6c8ed6f59f288745043ebba2a05aa83e9d
LangusMcgee <77115641+LangusMcgee@users.noreply.github.com>
2024-04-18 16:20:08 +0100

    Version 0.1

---
+++ FlicketyClank/Assets/Scripts/Boostoncollide.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boostoncollide : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float forceval = 10000f;
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+
+    void OnTriggerEnter(Collider other)
+    {
+        other.gameObject.GetComponent<Rigidbody>().AddForce(transform.forward * forceval * Time.deltaTime);
+        Debug.Log("Trigger Entered");
+        //other.Addforce(-cam.transform.forward * torque_amount * Time.deltaTime);
+        
+    }
+}
